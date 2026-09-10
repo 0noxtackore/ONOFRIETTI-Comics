0906e218e478861414cab40eaf9ae99f6e3ecc7f
0noxtackore <angelloxon@gmail.com>
2026-09-10 10:11:17 -0400

    feat: add subtle scale up effect on comic card hover

--- src/components/comic/ComicCard.vue
+++ src/components/comic/ComicCard.vue
@@ -36,7 +36,7 @@ const titleFontSize = computed(() => {
 </script>
 
 <template>
-  <article v-reveal="{ delay: (index % 3) * 90 }" class="group flex flex-col">
+  <article v-reveal="{ delay: (index % 3) * 90 }" class="group flex flex-col transition-transform duration-300 ease-out hover:scale-[1.03]">
     <a
       :href="`/comics/${comic.slug}`"
       :aria-label="`View ${comic.title} details`"
